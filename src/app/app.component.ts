8f383491d1619ae459acd52db04fa3ef30c80656
Jimoh Hadi <johadi@Jimohs-MacBook-Pro.local>
2018-11-22 13:56:45 +0100

    Finish up the main implementation for the validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-reactive-validation';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  formSubmitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+    this.setUserCategoryValidators();
+  }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required]],
+      username: [null, [Validators.required]],
+      userCategory: ['employee'],
+      institution: [null],
+      company: [null, [Validators.required]],
+      salary: [null, [Validators.required]],
+    });
+  }
+
+  setUserCategoryValidators() {
+    const institutionControl = this.form.get('institution');
+    const companyControl = this.form.get('company');
+    const salaryControl = this.form.get('salary');
+
+    this.form.get('userCategory').valueChanges
+      .subscribe(userCategory => {
+
+        if (userCategory === 'student') {
+          institutionControl.setValidators([Validators.required]);
+          companyControl.setValidators(null);
+          salaryControl.setValidators(null);
+        }
+
+        if (userCategory === 'employee') {
+          institutionControl.setValidators(null);
+          companyControl.setValidators([Validators.required]);
+          salaryControl.setValidators([Validators.required]);
+        }
+
+        institutionControl.updateValueAndValidity();
+        companyControl.updateValueAndValidity();
+        salaryControl.updateValueAndValidity();
+      });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.formSubmitted = true;
+
+    if (this.form.valid) {
+      console.log(this.form.value); // Process your form
+    }
+  }
 }
